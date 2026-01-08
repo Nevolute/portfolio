5c5eafa178ae1367c225b7da7c89f4bceb905475
Abhinav Nath Gupta <abhi.aec89@gmail.com>
2026-01-08 21:01:42 +0530

    fixes

--- next.config.ts
+++ next.config.ts
@@ -4,8 +4,6 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? '/portfolio' : '',
-  assetPrefix: isProd ? '/portfolio' : '',
   images: {
     unoptimized: true,
   },
